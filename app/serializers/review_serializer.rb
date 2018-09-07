e61c93cdce912dee30e197bc822259c74028c4a6
github username <github email address>
2018-09-07 13:06:22 -0400

    Create the componentes files

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,5 +1,5 @@
 class ReviewSerializer < ActiveModel::Serializer
-  attributes :id, :comment, :rating, :date
+  attributes :id, :comment, :rating, :date, :user_id
   belongs_to :truck
   belongs_to :user
 end
